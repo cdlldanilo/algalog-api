c68ea23c127847cb4423ee74b7a2c97a0b286b35
Danilo Cardilli <danilocardilli@gmail.com>
2022-06-14 16:18:24 -0300

    Execução do projeto, pasta dia01 foi com sucesso

---
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -0,0 +1,32 @@
+package com.algaworks.algalog.api.controller;
+
+import com.algaworks.algalog.domain.model.Cliente;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+public class ClienteController {
+
+    @GetMapping("/clientes")
+    public List<Cliente> listar() {
+
+        var cliente1 = new Cliente();
+        cliente1.setId(1L);
+        cliente1.setNome("João");
+        cliente1.setTelefone("11 99999-11111");
+        cliente1.setEmail("contato@yahoo.com.br");
+
+        var cliente2 = new Cliente();
+        cliente2.setId(2L);
+        cliente2.setNome("Maria");
+        cliente2.setTelefone("22 99999-22222");
+        cliente2.setEmail("contato@gmail.com");
+
+        return Arrays.asList(cliente1, cliente2);
+
+    }
+
+}
