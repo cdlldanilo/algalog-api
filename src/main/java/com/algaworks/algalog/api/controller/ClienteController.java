efdc44d0381e3768016a62521d563c84accd9311
Danilo Cardilli <danilocardilli@gmail.com>
2022-06-18 13:11:51 -0300

    Adicionar clientes foi executado com sucesso

--- src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -3,11 +3,9 @@ package com.algaworks.algalog.api.controller;
 import com.algaworks.algalog.domain.model.Cliente;
 import com.algaworks.algalog.domain.repository.ClienteRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -31,6 +29,14 @@ public class ClienteController {
         return clienteRepository.findById(clienteId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
+
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@RequestBody Cliente cliente) {
+
+        return clienteRepository.save(cliente);
 
     }
 
